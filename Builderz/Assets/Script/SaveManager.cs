03da9130234feb3447face303921f7ab69753a5d
CsecsiAndras <csecsi.andris@gmail.com>
2017-11-19 03:04:24 +0100

    Menu elements Oktatas prezentacio

---
+++ Builderz/Assets/Script/SaveManager.cs
@@ -0,0 +1,47 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SaveManager : MonoBehaviour
+{
+    public GameObject saveMenu;
+    public GameObject confirmMenu;
+
+    public Transform saveList;
+    public GameObject savePrefab;
+
+
+    public void OnSaveMenuClick()
+    {
+        saveMenu.SetActive(true);
+    }
+
+    public void OnSaveClick()
+    {
+        saveMenu.SetActive(false);
+        confirmMenu.SetActive(true);
+    }
+
+    public void OnLoadClick()
+    {
+        saveMenu.SetActive(false);
+        confirmMenu.SetActive(true);
+    }
+
+    public void OnCancelClick()
+    {
+        saveMenu.SetActive(false);
+    }
+
+    public void OnConfirmOK()
+    {
+        confirmMenu.SetActive(false);
+    }
+
+    public void OnConfirmCancel()
+    {
+        confirmMenu.SetActive(false);
+    }
+
+
+}
