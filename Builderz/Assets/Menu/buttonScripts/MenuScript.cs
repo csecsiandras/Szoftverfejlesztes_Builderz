d43fe362c821d4d7ed5977c91e84871f488f318b
David Horvath <noctisbrah@gmail.com>
2017-12-08 18:15:18 +0100

    Főmenü háttérzene + hangefektek

--- Builderz/Assets/Menu/buttonScripts/MenuScript.cs
+++ Builderz/Assets/Menu/buttonScripts/MenuScript.cs
@@ -5,19 +5,28 @@ using UnityEngine.SceneManagement;
 
 public class MenuScript : MonoBehaviour {
 
+	public AudioSource audio;
 	
 	public void StartGame(string SceneName)
 	{
+		PlayClickSound();
 		SceneManager.LoadScene(SceneName);
 	}
 
 	public void RateUs(string url)
 	{
+		PlayClickSound();
 		Application.OpenURL(url);
 	}
 
 	public void QuitGame()
 	{
+		PlayClickSound();
 		Application.Quit();
+	}
+	
+	public void PlayClickSound()
+	{
+		audio.Play();
 	}
 }
