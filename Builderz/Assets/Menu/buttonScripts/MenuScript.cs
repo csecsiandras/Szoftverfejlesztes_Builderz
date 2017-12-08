847ade0e5b60e65c0979f94aa6861e53324529e1
David Horvath <noctisbrah@gmail.com>
2017-12-08 14:24:59 +0100

    Menu gomb események bekötése; javítások

---
+++ Builderz/Assets/Menu/buttonScripts/MenuScript.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class MenuScript : MonoBehaviour {
+
+	
+	public void StartGame(string SceneName)
+	{
+		SceneManager.LoadScene(SceneName);
+	}
+
+	public void RateUs(string url)
+	{
+		Application.OpenURL(url);
+	}
+
+	public void QuitGame()
+	{
+		Application.Quit();
+	}
+}
